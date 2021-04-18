ec44392826ba276ed411388c27c8afec3eee7bae
Jere Pesälä <j.pesala@protonmail.com>
2021-04-19 01:17:22 +0300

    Draft for Main page. No functionality coded.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import './App.css'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+
+  return(
+      <div className="grid">
+
+          <div className="logo">
+              SääApp
+          </div>
+
+
+          <input className="searchInput" type="text" size="50" />
+          <button className="searchButton"> Hae </button>
+
+              <div className="w1">
+                  Sää 1
+              </div>
+          <div className="w2">
+                  Sää 2
+              </div>
+          <div className="w3">
+                  Sää 3
+              </div>
+          <div className="w4">
+                  Sää 4
+              </div>
+          <div className="w5">
+                  Sää 5
+              </div>
+          <div className="w6">
+                  Sää 6
+              </div>
+
+          <img className="map" alt="WeatherMap" src="https://cdn.fmi.fi/weather-observations/products/finland/frontpage_wx_obs-latest.png?timestamp=0" >
+
+
+          </img>
+
+
+
+
+      </div>
+  )
+
+}
 export default App;
