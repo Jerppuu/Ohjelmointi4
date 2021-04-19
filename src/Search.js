5c61b7a3299489c1e30b285d1ac9d3f4fc69ee19
Akke-Pekka <aptorman@protonmail.com>
2021-04-19 16:01:34 +0300

    datalist supports all finnish municipalities

--- src/Search.js
+++ src/Search.js
@@ -1,15 +1,22 @@
 import './Search.css';
+import Parser from 'html-react-parser';
+
+import kuntalista from './other/kunnat'
+
+let options = '';
+for (let i = 0; i < kuntalista.length; i++) {
+	options += '<option value="' + kuntalista[i] + '"/>\n';
+}
 
 function Search() {
 	return (
 		<div className="column">
-			<label for={"valitse-paikkakunta"}>Valitse paikkakunta:</label>
+			<label htmlFor={"valitse-paikkakunta"}>Valitse paikkakunta:</label>
 			<input list={"paikkakunta-lista"} id={"valitse-paikkakunta"}/>
 			<datalist id={"paikkakunta-lista"}>
-				<option value={"kuusamo"}/>
-				<option value={"oulu"}/>
-				<option value={"helsinki"}/>
-			</datalist><button>Hae</button>
+				{Parser(options)}
+			</datalist>
+			<button>Hae</button>
 		</div>
 	);
 }
