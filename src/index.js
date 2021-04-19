15e8e002bdb5174c741555e10449e4b502d00909
Jere Pesälä <j.pesala@protonmail.com>
2021-04-19 10:56:20 +0300

    Merged Akke's sprint 1.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import Search from './Search';
 import WeekView from './WeekView.js';
 import Map from './Map';
 
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
     <React.StrictMode>
@@ -39,7 +38,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
