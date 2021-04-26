aa691129240d501ed1823deab2b5c8255e8134ec
Akke-Pekka <aptorman@protonmail.com>
2021-04-26 11:45:18 +0300

    implement datalist with finnish municapilities

--- client/src/Search.js
+++ client/src/Search.js
@@ -1,12 +1,18 @@
+import Parser from 'html-react-parser';
+
+import kuntalista from './other/kunnat'
+
+let options = '';
+for (let i = 0; i < kuntalista.length; i++) {
+	options += '<option value="' + kuntalista[i] + '"/>\n';
+}
 
 function Search() {
 	return (
 		<div>
 			<input list={"paikkakunta-lista"} id={"valitse-paikkakunta"}/>
 			<datalist id={"paikkakunta-lista"}>
-				<option value={"kuusamo"}/>
-				<option value={"oulu"}/>
-				<option value={"helsinki"}/>
+				{Parser(options)}
 			</datalist><button>Hae</button>
 		</div>
 	);
