b4e8e5ba22693dfd9d2f7695797895bfe795e22e
Jere Pesälä <j.pesala@protonmail.com>
2021-04-26 09:35:19 +0300

    Added day view with hours, with ability to switch between week and day view

--- src/WeekView.js
+++ src/WeekView.js
@@ -4,12 +4,12 @@ function WeekView() {
 	return (
 		<div>
 			<div className="weekdays">
-				<div className="day" style={{height: "100px", width: "75px", backgroundColor: "#ddd"}}>Ma</div>
-				<div className="day" style={{height: "100px", width: "75px",backgroundColor: "#ccc"}}>Ti</div>
-				<div className="day" style={{height: "100px", width: "75px",backgroundColor: "#bbb"}}>Ke</div>
-				<div className="day" style={{height: "100px", width: "75px",backgroundColor: "#aaa"}}>To</div>
-				<div className="day" style={{height: "100px", width: "75px",backgroundColor: "#999"}}>Pe</div>
-				<div className="day" style={{height: "100px", width: "75px",backgroundColor: "#888"}}>La</div>
+				<div className="day">Ma</div>
+				<div className="day">Ti</div>
+				<div className="day">Ke</div>
+				<div className="day">To</div>
+				<div className="day">Pe</div>
+				<div className="day">La</div>
 			</div>
 			<div className="nav" style={{gap: "10px", justifyContent: "space-evenly"}}>
 				<div style={{backgroundColor: "lightblue"}}>edellinen</div>
--- src/index.js
+++ src/index.js
@@ -1,48 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
-import TodayPreview from './TodayPreview';
-import Search from './Search';
-import WeekView from './WeekView.js';
-import Map from './Map';
-import Logo from "./Logo";
-
+import App from "./App";
 
 ReactDOM.render(
-    <React.StrictMode>
-        <body>
-            <div className="header">
-                    <Logo/>
-                    <Search/>
-            </div>
-
-            <div className="mainBody">
-                <div className="leftSide">
-                    <TodayPreview/>
-                    <WeekView/>
-                </div>
-                <Map/>
-
-            </div>
-
-
-
-            <nav className="navbar navbar-expand-sm bg-primary navbar-dark" style={{position: "fixed",bottom: 0, width: "100%", height: "30px"}}>
-                <ul className="navbar-nav">
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Meistä</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Ohjeet</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Sivustokartta</a>
-                    </li>
-                </ul>
-            </nav>
-        </body>
-    </React.StrictMode>,
+    <App/>,
   document.getElementById('root')
 );
 
