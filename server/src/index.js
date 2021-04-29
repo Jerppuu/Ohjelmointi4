0047c7b1d307400da75494be0bebd29b8443ee5e
Akke-Pekka <aptorman@protonmail.com>
2021-04-29 16:09:06 +0300

    unsecure api request relay via server backend between client and foreca server

--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 3001;
 const fetch = require("node-fetch");
 const dummy = require("./other/dummy.json");
+const path = require("path");
 /*
 	dummy structure:
 		{
@@ -26,37 +27,48 @@ const dummy = require("./other/dummy.json");
 						]
 		}
  */
-var token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9wZmEuZm9yZWNhLmNvbVwvYXV0aG9yaXplXC90b2tlbiIsImlhdCI6MTYxOTY3NjQ2OSwiZXhwIjoxNjE5NjgzNjY5LCJuYmYiOjE2MTk2NzY0NjksImp0aSI6ImE4MTE5NDBmYjY5ZTM5MDEiLCJzdWIiOiJha2tlcGVra2EiLCJmbXQiOiJYRGNPaGpDNDArQUxqbFlUdGpiT2lBPT0ifQ.f0Ex6NAK_MOxYwTXLGEDBhB1i331deK_LfPPnrFS9-w";
-var responseJSON = {
-						"forecast":[
-							"daily", "hourly"
-						]
-					};
+var token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9wZmEuZm9yZWNhLmNvbVwvYXV0aG9yaXplXC90b2tlbiIsImlhdCI6MTYxOTY5NzA5OSwiZXhwIjoxNjE5NzA0Mjk5LCJuYmYiOjE2MTk2OTcwOTksImp0aSI6IjA4ODdmNzcxODJiMWU4MzIiLCJzdWIiOiJha2tlcGVra2EiLCJmbXQiOiJYRGNPaGpDNDArQUxqbFlUdGpiT2lBPT0ifQ.3l5sIBBFvvoazNhwYumklRK9VhlGhH4rzNtYxu5L_L0";
 
-app.get('/api/:word', (req, res) => {
-	res.json(dummy); // uncomment to send dummy.json for dev purposes
 
-	// proper response below in the works, does not compile
-	/*
+app.get('/api/search/:location', (req, res) => {
+	//res.json(dummy); // uncomment to send dummy.json for dev purposes
+
+	// proper response below in the works, atm uses the id of first hit i gets from locations
+	var responseJSON = {"forecast": []};
 	try {
-		getLocation(req.params.word, token)
-			.then(result => {
+		getLocation(req.params.location, token)
+			.then(async (result) => {
 				let id = result;
-				responseJSON.forecast.daily.append(getDaily(id, token).forecast)
-					.then(() => responseJSON.forecast.hourly.append(getHourly(id, token).forecast))
-			}).then(result => res.json(result));
+			//	console.log(id);
+				if (id === 0) {
+					res.json("notfound");
+					return;
+				}
+				let daily = await getDaily(id,token);
+			//	console.log(JSON.stringify(daily));
+				let hourly = await getHourly(id,token);
+			//	console.log(JSON.stringify(hourly));
+				responseJSON.forecast.push({"daily": daily});
+				responseJSON.forecast.push({"hourly": hourly});
+			}).then(result => {
+			//console.log(JSON.stringify(responseJSON));
+			res.json(responseJSON);
+			});
 	} catch(error) {
 		console.log('error: ', error);
 		res.code(400).end();
 
 	}
-	*/
+
 });
+
+app.use(express.static(path.join(__dirname, "../../client", "./build")));
 
 app.listen(port, () => {
 	console.log(`Express app listening at http://localhost:${port}`);
 });
 
+// atm uses the id of first hit i gets from locations
 async function getLocation(municipality, token) {
 	let requestOptions = {
 		method: 'GET',
@@ -66,9 +78,15 @@ async function getLocation(municipality, token) {
 		redirect: 'follow'
 	};
 	try {
+		// if you need more detailed dataset use argument: ?dataset=full
 		return fetch("https://pfa.foreca.com/api/v1/location/search/" + municipality, requestOptions)
 			.then(response => response.json())
-			.then(data => data.locations[0].id)
+			.then(data => {
+				if (data.locations[0].name === municipality)
+					return data.locations[0].id;
+				else
+					return 0;
+			})
 			.catch(error =>
 				console.log('error: ', error));
 	} catch (error) {
@@ -76,7 +94,7 @@ async function getLocation(municipality, token) {
 	}
 }
 
-function getDaily(id, token) {
+async function getDaily(id, token) {
 	let requestOptions = {
 		method: 'GET',
 		headers: {
@@ -84,12 +102,17 @@ function getDaily(id, token) {
 		},
 		redirect: 'follow'
 	};
-
-	return fetch("https://pfa.foreca.com/api/v1/forecast/daily/" + id +"?periods=15?dataset=full", requestOptions).json().forecast
+	// if you need more detailed dataset use argument: ?dataset=full
+	return fetch("https://pfa.foreca.com/api/v1/forecast/daily/" + id +"?periods=15", requestOptions)
+		.then(async result => {
+			let promise = result.json();
+			let data = await promise;
+			return data.forecast;
+		})
 		.catch(error => console.log('error', error));
 }
 
-function getHourly(id, token) {
+async function getHourly(id, token) {
 
 	let requestOptions = {
 		method: 'GET',
@@ -99,6 +122,11 @@ function getHourly(id, token) {
 		redirect: 'follow'
 	};
 
-	return fetch("https://pfa.foreca.com/api/v1/forecast/hourly/" + id +"?periods=169?dataset=full", requestOptions).json().forecast
+	return fetch("https://pfa.foreca.com/api/v1/forecast/hourly/" + id +"?periods=169", requestOptions)
+		.then(async result => {
+			let promise = result.json();
+			let data = await promise;
+			return data.forecast;
+		})
 		.catch(error => console.log('error', error));
 }
